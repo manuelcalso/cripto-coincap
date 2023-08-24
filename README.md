16ed19875dec7904247ac2479e6507b9746ff92c
JUAN manuel calleros <116044083+manuelcalso@users.noreply.github.com>
2023-08-23 20:34:01 -0600

    primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+api utilizada
+https://docs.coincap.io/#a549b4e3-544a-407e-aacd-8b44630c505a
---
+++ main.js
@@ -0,0 +1,59 @@
+import "./style.css"
+
+const mostrar = document.querySelector('#app')
+
+ 
+
+ 
+
+ 
+
+//EVENTOS
+
+ 
+
+document.addEventListener('DOMContentLoaded', buscarCripto);
+
+ 
+
+ 
+
+//FUNCIONES
+
+async function buscarCripto(){
+  let url = "https://api.coincap.io/v2/assets"
+
+    try {
+
+        const respuestaRaw = await fetch(url)
+        const criptomonedas = await respuestaRaw.json()
+
+        imprimir(criptomonedas.data)
+
+    } catch (error) {
+
+        console.log(error)
+
+    }    
+
+}
+
+
+function imprimir(arregloPuro){
+  
+  mostrar.innerHTML = arregloPuro.map((cripto)=>{
+    let precioCripto = Number(cripto.priceUsd)
+    let precioFormateado = precioCripto.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+  return `
+  <div class="content">
+  <h1 class="criptoname">${cripto.id}</h1>
+  <h3>current price: ${precioFormateado}</h3>
+  </div>
+  `
+  }).join("</hr>")
+}
